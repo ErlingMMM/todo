d806bfa6758eff1d1c051eca45df1fdabdcb4446
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2025-04-22 22:45:03 +0200

    toggle btn

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const input = document.getElementById("todo-input");
 const addBtn = document.getElementById("add-btn");
 const filterSelect = document.getElementById("filter-select");
 const dateInput = document.getElementById("todo-deadline");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
 
 function addTask() {
   const newTask = input.value.trim();
@@ -26,6 +27,17 @@ input.addEventListener("keypress", (e) => {
 filterSelect.addEventListener("change", () => {
   setCurrentFilter(filterSelect.value);
   renderList();
+});
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  const isDark = document.body.classList.contains("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
 });
 
 renderList();
